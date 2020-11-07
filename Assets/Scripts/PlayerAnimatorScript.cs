4a469874fc2375f596b1fbdfa20d4800b5982f40
Samuel Bertrand <soccer-samuel@hotmail.com>
2020-11-07 16:16:54 -0500

    Add Characters + can switch between

---
+++ Assets/Scripts/PlayerAnimatorScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimatorScript : MonoBehaviour
+{
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else{anim.SetBool("isWalking", false); }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("isJumping", true);
+        }
+        else { anim.SetBool("isJumping", false); }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else { anim.SetBool("isRunning", false); }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isBackward", true);
+        }
+        else { anim.SetBool("isBackward", false); }
+    }
+}
---
+++ Assets/Scripts/SwitchCharacters.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCharacters : MonoBehaviour
+{
+    public GameObject Character1, Character2;
+    int currentCaracter;
+    void Start()
+    {
+        currentCaracter = 1;
+
+        Character1.GetComponent<PlayerMovementScript>().enabled = true;
+        Character2.GetComponent<PlayerMovementScript>().enabled = false;
+
+        Character1.GetComponentInChildren<PlayerAnimatorScript>().enabled = true;
+        Character2.GetComponentInChildren<PlayerAnimatorScript>().enabled = false;
+
+        Character1.GetComponentInChildren<MouseLook>().enabled = true;
+        Character2.GetComponentInChildren<MouseLook>().enabled = false;
+
+        Character1.GetComponentInChildren<Camera>().enabled = true;
+        Character2.GetComponentInChildren<Camera>().enabled = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+         SwitchCharacter(1);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SwitchCharacter(2);
+    }
+    public void SwitchCharacter(int number)
+    {
+        switch(number)
+        {
+            case 1:
+
+                currentCaracter = 1;
+
+                Character1.GetComponent<PlayerMovementScript>().enabled = true;
+                Character2.GetComponent<PlayerMovementScript>().enabled = false;
+
+                Character1.GetComponentInChildren<PlayerAnimatorScript>().enabled = true;
+                Character2.GetComponentInChildren<PlayerAnimatorScript>().enabled = false;
+
+                Character1.GetComponentInChildren<MouseLook>().enabled = true;
+                Character2.GetComponentInChildren<MouseLook>().enabled = false;
+
+                Character1.GetComponentInChildren<Camera>().enabled = true;
+                Character2.GetComponentInChildren<Camera>().enabled = false;
+
+                Toast.Instance.Show("Switched to 1rst Character", 2f);
+                break;
+
+            case 2:
+
+                currentCaracter = 2;
+
+                Character1.GetComponent<PlayerMovementScript>().enabled = false;
+                Character2.GetComponent<PlayerMovementScript>().enabled = true;
+
+                Character1.GetComponentInChildren<PlayerAnimatorScript>().enabled = false;
+                Character2.GetComponentInChildren<PlayerAnimatorScript>().enabled = true;
+
+                Character1.GetComponentInChildren<MouseLook>().enabled = false;
+                Character2.GetComponentInChildren<MouseLook>().enabled = true;
+
+                Character1.GetComponentInChildren<Camera>().enabled = false;
+                Character2.GetComponentInChildren<Camera>().enabled = true;
+
+                Toast.Instance.Show("Switched to 2nd Character", 2f);
+                break;
+        }
+    }
+}
